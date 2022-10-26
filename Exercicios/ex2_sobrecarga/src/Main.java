89d824aa7c44cbc4db2f58253e305c32b2565ccb
rafaelramon <rafael-kadu-ramon@hotmail.com>
2022-10-26 14:55:56 -0300

    add ex2_sobrecarga

---
+++ Exercicios/ex2_sobrecarga/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Calcular quadrado
+       Quadrilateros.calculaArea(5);
+
+       //Calcular retângulo
+        Quadrilateros.calculaArea(7,7);
+
+        // Calcular trapézio
+        Quadrilateros.calculaArea(7,8,9);
+
+    }
+}
